e8f501e70b873c138cd2145d69df4d89cac234d8
pieter <pieter.martin@gmail.com>
2014-08-11 15:06:50 +0200

    Start add test for nonexistent property

---
+++ sqlg-test/src/main/java/org/umlg/sqlg/test/TestGetterOnNoExistentProperty.java
@@ -0,0 +1,16 @@
+package org.umlg.sqlg.test;
+
+import org.junit.Test;
+
+/**
+ * Date: 2014/08/11
+ * Time: 10:55 AM
+ */
+public class TestGetterOnNoExistentProperty extends BaseTest {
+
+    @Test
+    public void testNonExistentProperty() {
+
+
+    }
+}
