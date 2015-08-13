0f24254b2bf1dd7d853c58ec9ee72369dc26cf11
pieter <pieter.martin@gmail.com>
2015-08-13 22:54:02 +0200

    rename Text.eq to contains

--- sqlg-core/src/main/java/org/umlg/sqlg/predicate/Text.java
+++ sqlg-core/src/main/java/org/umlg/sqlg/predicate/Text.java
@@ -10,30 +10,30 @@ import java.util.function.BiPredicate;
  */
 public enum Text implements BiPredicate<String, String> {
 
-    eq {
+    contains {
         @Override
         public boolean test(final String first, final String second) {
-            Preconditions.checkState(first != null && second != null, "Test.eq may not be called with a null value.");
-           return first.contains(second);
+            Preconditions.checkState(first != null && second != null, "Test.contains may not be called with a null value.");
+            return first.contains(second);
         }
 
         @Override
         public Text negate() {
-            return neq;
+            return ncontains;
         }
-    }, neq {
+    }, ncontains {
         @Override
         public boolean test(final String first, final String second) {
-            return !eq.test(first, second);
+            return !contains.test(first, second);
         }
 
         @Override
         public Text negate() {
-            return eq;
+            return contains;
         }
     };
 
     public static P<String> eq(final String value) {
-        return new P(Text.eq, value);
+        return new P(Text.contains, value);
     }
 }
