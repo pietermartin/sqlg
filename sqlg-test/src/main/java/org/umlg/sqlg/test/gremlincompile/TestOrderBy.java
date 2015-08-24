ce4a2216d7c0d8ef713bec1bd7cc00d64294d045
pieter <pieter.martin@rorotika.com>
2015-08-24 10:49:36 +0200

    minor refactor

--- sqlg-test/src/main/java/org/umlg/sqlg/test/gremlincompile/TestOrderBy.java
+++ sqlg-test/src/main/java/org/umlg/sqlg/test/gremlincompile/TestOrderBy.java
@@ -1,13 +1,8 @@
 package org.umlg.sqlg.test.gremlincompile;
 
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
-
-import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.*;
-
 import org.apache.tinkerpop.gremlin.AbstractGremlinTest;
 import org.apache.tinkerpop.gremlin.process.traversal.Order;
 import org.apache.tinkerpop.gremlin.process.traversal.Traversal;
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
 import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.apache.tinkerpop.gremlin.structure.T;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
@@ -24,34 +19,36 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.select;
+
 /**
  * Created by pieter on 2015/08/22.
  */
 public class TestOrderBy extends BaseTest {
 
-//    @Test
-//    public void testOrderBy() {
-//        Vertex a1 = this.sqlgGraph.addVertex(T.label, "A", "name", "a", "surname", "a");
-//        Vertex a2 = this.sqlgGraph.addVertex(T.label, "A", "name", "a", "surname", "b");
-//        Vertex a3 = this.sqlgGraph.addVertex(T.label, "A", "name", "a", "surname", "c");
-//        Vertex b1 = this.sqlgGraph.addVertex(T.label, "A", "name", "b", "surname", "a");
-//        Vertex b2 = this.sqlgGraph.addVertex(T.label, "A", "name", "b", "surname", "b");
-//        Vertex b3 = this.sqlgGraph.addVertex(T.label, "A", "name", "b", "surname", "c");
-//        this.sqlgGraph.tx().commit();
-//
-//        List<Vertex> result = this.sqlgGraph.traversal().V().hasLabel("A")
-//                .order()
-//                .by("name", Order.incr).by("surname", Order.decr)
-//                .toList();
-//
-//        Assert.assertEquals(6, result.size());
-//        Assert.assertEquals(a3, result.get(0));
-//        Assert.assertEquals(a2, result.get(1));
-//        Assert.assertEquals(a1, result.get(2));
-//        Assert.assertEquals(b3, result.get(3));
-//        Assert.assertEquals(b2, result.get(4));
-//        Assert.assertEquals(b1, result.get(5));
-//    }
+    @Test
+    public void testOrderBy() {
+        Vertex a1 = this.sqlgGraph.addVertex(T.label, "A", "name", "a", "surname", "a");
+        Vertex a2 = this.sqlgGraph.addVertex(T.label, "A", "name", "a", "surname", "b");
+        Vertex a3 = this.sqlgGraph.addVertex(T.label, "A", "name", "a", "surname", "c");
+        Vertex b1 = this.sqlgGraph.addVertex(T.label, "A", "name", "b", "surname", "a");
+        Vertex b2 = this.sqlgGraph.addVertex(T.label, "A", "name", "b", "surname", "b");
+        Vertex b3 = this.sqlgGraph.addVertex(T.label, "A", "name", "b", "surname", "c");
+        this.sqlgGraph.tx().commit();
+
+        List<Vertex> result = this.sqlgGraph.traversal().V().hasLabel("A")
+                .order()
+                .by("name", Order.incr).by("surname", Order.decr)
+                .toList();
+
+        Assert.assertEquals(6, result.size());
+        Assert.assertEquals(a3, result.get(0));
+        Assert.assertEquals(a2, result.get(1));
+        Assert.assertEquals(a1, result.get(2));
+        Assert.assertEquals(b3, result.get(3));
+        Assert.assertEquals(b2, result.get(4));
+        Assert.assertEquals(b1, result.get(5));
+    }
 
     @Test
     public void testOrderBy2() {
@@ -133,72 +130,72 @@ public class TestOrderBy extends BaseTest {
         Assert.assertEquals("RNCA", row8.get("nn").value("name"));
     }
 
-//    @Test
-//    public void testOrderby3() {
-//        Vertex a1 = this.sqlgGraph.addVertex(T.label, "A", "name", "aa");
-//        Vertex a2 = this.sqlgGraph.addVertex(T.label, "A", "name", "ab");
-//        Vertex b1 = this.sqlgGraph.addVertex(T.label, "B", "name", "ba");
-//        Vertex b2 = this.sqlgGraph.addVertex(T.label, "B", "name", "bb");
-//        Vertex b3 = this.sqlgGraph.addVertex(T.label, "B", "name", "bc");
-//        Vertex b4 = this.sqlgGraph.addVertex(T.label, "B", "name", "bd");
-//        a1.addEdge("ab", b1);
-//        a1.addEdge("ab", b2);
-//        a2.addEdge("ab", b3);
-//        a2.addEdge("ab", b4);
-//        this.sqlgGraph.tx().commit();
-//
-//        List<Map<String, Vertex>> result = this.sqlgGraph.traversal().V()
-//                .hasLabel("A").as("a")
-//                .out("ab").as("b")
-//                .<Vertex>select("a", "b")
-//                .order()
-//                .by(select("a").by("name"), Order.incr)
-//                .by(select("b").by("name"), Order.decr)
-//                .toList();
-//
-//        Assert.assertEquals(4, result.size());
-//        Map<String, Vertex> map1 = result.get(0);
-//        Map<String, Vertex> map2 = result.get(1);
-//        Map<String, Vertex> map3 = result.get(2);
-//        Map<String, Vertex> map4 = result.get(3);
-//        Assert.assertEquals("aa", map1.get("a").value("name"));
-//        Assert.assertEquals("bb", map1.get("b").value("name"));
-//        Assert.assertEquals("aa", map2.get("a").value("name"));
-//        Assert.assertEquals("ba", map2.get("b").value("name"));
-//        Assert.assertEquals("ab", map3.get("a").value("name"));
-//        Assert.assertEquals("bd", map3.get("b").value("name"));
-//        Assert.assertEquals("ab", map4.get("a").value("name"));
-//        Assert.assertEquals("bc", map4.get("b").value("name"));
-//    }
-//
-//    @Test
-//    public void g_V_order_byXname_incrX_name() throws IOException {
-//        Graph g = this.sqlgGraph;
-//        final GraphReader reader = GryoReader.build()
-//                .mapper(g.io(GryoIo.build()).mapper().create())
-//                .create();
-//        try (final InputStream stream = AbstractGremlinTest.class.getResourceAsStream("/tinkerpop-modern.kryo")) {
-//            reader.readGraph(stream, g);
-//        }
-//        assertModernGraph(g, true, false);
-//        Arrays.asList(get_g_V_order_byXname_incrX_name()/*, get_g_V_order_byXnameX_name()*/).forEach(traversal -> {
-//            printTraversalForm(traversal);
-//            final List<String> names = traversal.toList();
-//            Assert.assertEquals(names.size(), 6);
-//            Assert.assertEquals("josh", names.get(0));
-//            Assert.assertEquals("lop", names.get(1));
-//            Assert.assertEquals("marko", names.get(2));
-//            Assert.assertEquals("peter", names.get(3));
-//            Assert.assertEquals("ripple", names.get(4));
-//            Assert.assertEquals("vadas", names.get(5));
-//        });
-//    }
-//
-//    public Traversal<Vertex, String> get_g_V_order_byXname_incrX_name() {
-//        return this.sqlgGraph.traversal().V().order().by("name", Order.incr).values("name");
-//    }
-//
-//    public Traversal<Vertex, String> get_g_V_order_byXnameX_name() {
-//        return this.sqlgGraph.traversal().V().order().by("name", Order.incr).values("name");
-//    }
+    @Test
+    public void testOrderby3() {
+        Vertex a1 = this.sqlgGraph.addVertex(T.label, "A", "name", "aa");
+        Vertex a2 = this.sqlgGraph.addVertex(T.label, "A", "name", "ab");
+        Vertex b1 = this.sqlgGraph.addVertex(T.label, "B", "name", "ba");
+        Vertex b2 = this.sqlgGraph.addVertex(T.label, "B", "name", "bb");
+        Vertex b3 = this.sqlgGraph.addVertex(T.label, "B", "name", "bc");
+        Vertex b4 = this.sqlgGraph.addVertex(T.label, "B", "name", "bd");
+        a1.addEdge("ab", b1);
+        a1.addEdge("ab", b2);
+        a2.addEdge("ab", b3);
+        a2.addEdge("ab", b4);
+        this.sqlgGraph.tx().commit();
+
+        List<Map<String, Vertex>> result = this.sqlgGraph.traversal().V()
+                .hasLabel("A").as("a")
+                .out("ab").as("b")
+                .<Vertex>select("a", "b")
+                .order()
+                .by(select("a").by("name"), Order.incr)
+                .by(select("b").by("name"), Order.decr)
+                .toList();
+
+        Assert.assertEquals(4, result.size());
+        Map<String, Vertex> map1 = result.get(0);
+        Map<String, Vertex> map2 = result.get(1);
+        Map<String, Vertex> map3 = result.get(2);
+        Map<String, Vertex> map4 = result.get(3);
+        Assert.assertEquals("aa", map1.get("a").value("name"));
+        Assert.assertEquals("bb", map1.get("b").value("name"));
+        Assert.assertEquals("aa", map2.get("a").value("name"));
+        Assert.assertEquals("ba", map2.get("b").value("name"));
+        Assert.assertEquals("ab", map3.get("a").value("name"));
+        Assert.assertEquals("bd", map3.get("b").value("name"));
+        Assert.assertEquals("ab", map4.get("a").value("name"));
+        Assert.assertEquals("bc", map4.get("b").value("name"));
+    }
+
+    @Test
+    public void g_V_order_byXname_incrX_name() throws IOException {
+        Graph g = this.sqlgGraph;
+        final GraphReader reader = GryoReader.build()
+                .mapper(g.io(GryoIo.build()).mapper().create())
+                .create();
+        try (final InputStream stream = AbstractGremlinTest.class.getResourceAsStream("/tinkerpop-modern.kryo")) {
+            reader.readGraph(stream, g);
+        }
+        assertModernGraph(g, true, false);
+        Arrays.asList(get_g_V_order_byXname_incrX_name()/*, get_g_V_order_byXnameX_name()*/).forEach(traversal -> {
+            printTraversalForm(traversal);
+            final List<String> names = traversal.toList();
+            Assert.assertEquals(names.size(), 6);
+            Assert.assertEquals("josh", names.get(0));
+            Assert.assertEquals("lop", names.get(1));
+            Assert.assertEquals("marko", names.get(2));
+            Assert.assertEquals("peter", names.get(3));
+            Assert.assertEquals("ripple", names.get(4));
+            Assert.assertEquals("vadas", names.get(5));
+        });
+    }
+
+    public Traversal<Vertex, String> get_g_V_order_byXname_incrX_name() {
+        return this.sqlgGraph.traversal().V().order().by("name", Order.incr).values("name");
+    }
+
+    public Traversal<Vertex, String> get_g_V_order_byXnameX_name() {
+        return this.sqlgGraph.traversal().V().order().by("name", Order.incr).values("name");
+    }
 }
