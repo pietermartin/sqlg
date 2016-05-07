67b314845882bd3bb31ccf39863ba82e3a9b8b18
pieter <pieter.martin@gmail.com>
2016-05-07 13:52:44 +0200

    added LocalStepTest

--- sqlg-test/src/main/java/org/umlg/sqlg/AnyTest.java
+++ sqlg-test/src/main/java/org/umlg/sqlg/AnyTest.java
@@ -2,7 +2,7 @@ package org.umlg.sqlg;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.umlg.sqlg.test.travers.TestTraversals;
+import org.umlg.sqlg.test.compilelocalstep.TestLocalStepCompile;
 
 /**
  * Date: 2014/07/16
@@ -10,7 +10,7 @@ import org.umlg.sqlg.test.travers.TestTraversals;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        TestTraversals.class,
+        TestLocalStepCompile.class,
 //        TestHas.class,
 })
 public class AnyTest {
---
+++ sqlg-test/src/main/java/org/umlg/sqlg/test/compilelocalstep/TestLocalStepCompile.java
@@ -0,0 +1,34 @@
+package org.umlg.sqlg.test.compilelocalstep;
+
+import org.apache.tinkerpop.gremlin.process.traversal.Path;
+import org.apache.tinkerpop.gremlin.structure.T;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.junit.Test;
+import org.umlg.sqlg.test.BaseTest;
+
+import java.util.List;
+
+import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.out;
+
+/**
+ * Date: 2016/05/07
+ * Time: 1:46 PM
+ */
+public class TestLocalStepCompile extends BaseTest {
+
+    @Test
+    public void test() {
+        Vertex a1 = this.sqlgGraph.addVertex(T.label, "A", "name", "a1");
+        Vertex a11 = this.sqlgGraph.addVertex(T.label, "A", "name", "a11");
+        Vertex b1 = this.sqlgGraph.addVertex(T.label, "B", "name", "b1");
+        Vertex c1 = this.sqlgGraph.addVertex(T.label, "C", "name", "c1");
+        a1.addEdge("ab", b1);
+        b1.addEdge("bc", c1);
+        this.sqlgGraph.tx().commit();
+
+        List<Path> paths = gt.V(a1).local(out().out()).path().toList();
+        for (Path path : paths) {
+            System.out.println(path);
+        }
+    }
+}
