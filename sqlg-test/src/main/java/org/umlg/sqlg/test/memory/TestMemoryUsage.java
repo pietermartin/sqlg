389f15895995b0ddcfa18a577055f94b36b60549
pieter <pieter.martin@gmail.com>
2016-05-15 12:06:15 +0200

    add in Assume batchMode

--- sqlg-test/src/main/java/org/umlg/sqlg/test/memory/TestMemoryUsage.java
+++ sqlg-test/src/main/java/org/umlg/sqlg/test/memory/TestMemoryUsage.java
@@ -1,6 +1,8 @@
 package org.umlg.sqlg.test.memory;
 
 import org.apache.tinkerpop.gremlin.structure.T;
+import org.junit.Assume;
+import org.junit.Before;
 import org.junit.Test;
 import org.umlg.sqlg.test.BaseTest;
 
@@ -12,6 +14,11 @@ import static org.junit.Assert.assertTrue;
  * Time: 8:22 AM
  */
 public class TestMemoryUsage extends BaseTest {
+
+    @Before
+    public void beforeTest() {
+        Assume.assumeTrue(this.sqlgGraph.getSqlDialect().supportsBatchMode());
+    }
 
     @Test
     public void testMemoryUsage() throws InterruptedException {
