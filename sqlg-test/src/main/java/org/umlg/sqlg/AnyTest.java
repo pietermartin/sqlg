b80d947174cedb44f1a36f45d33e6aa0a6188e1b
pieter <pieter.martin@gmail.com>
2016-07-09 20:20:29 +0200

    no real change

--- sqlg-test/src/main/java/org/umlg/sqlg/AnyTest.java
+++ sqlg-test/src/main/java/org/umlg/sqlg/AnyTest.java
@@ -2,7 +2,7 @@ package org.umlg.sqlg;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.umlg.sqlg.test.labels.TestMultipleLabels;
+import org.umlg.sqlg.test.gremlincompile.TestTraversalPerformance;
 
 /**
  * Date: 2014/07/16
@@ -10,7 +10,7 @@ import org.umlg.sqlg.test.labels.TestMultipleLabels;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        TestMultipleLabels.class
+        TestTraversalPerformance.class
 })
 public class AnyTest {
 }
