f6942c471ac1e6c0f5dffd06f9b5992efed10799
pieter <pieter.martin@gmail.com>
2016-07-11 07:40:04 +0200

    start looking at failing tests

--- sqlg-test/src/main/java/org/umlg/sqlg/test/rollback/TestRollback.java
+++ sqlg-test/src/main/java/org/umlg/sqlg/test/rollback/TestRollback.java
@@ -3,9 +3,10 @@ package org.umlg.sqlg.test.rollback;
 import org.apache.tinkerpop.gremlin.structure.T;
 import org.apache.tinkerpop.gremlin.structure.Transaction;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
-import org.junit.Assert;
 import org.junit.Test;
 import org.umlg.sqlg.test.BaseTest;
+
+import static org.junit.Assert.*;
 
 /**
  * Date: 2014/09/24
@@ -14,11 +15,21 @@ import org.umlg.sqlg.test.BaseTest;
 public class TestRollback extends BaseTest {
 
     @Test
+    public void shouldRollbackByDefault() {
+        Vertex a1 = this.sqlgGraph.addVertex(T.label, "A");
+        Vertex b1 = this.sqlgGraph.addVertex(T.label, "B");
+        a1.addEdge("ab", b1);
+//        this.sqlgGraph.tx().rollback();
+        this.sqlgGraph.traversal().tx().rollback();
+        assertFalse(this.sqlgGraph.edges().hasNext());
+    }
+
+    @Test
     public void shouldHaveExceptionConsistencyWhenUsingManualTransactionOnRollback() {
         this.sqlgGraph.tx().onReadWrite(Transaction.READ_WRITE_BEHAVIOR.MANUAL);
         try {
             this.sqlgGraph.tx().rollback();
-            Assert.fail("An exception should be thrown when read/write behavior is manual and no transaction is opened");
+            fail("An exception should be thrown when read/write behavior is manual and no transaction is opened");
         } catch (Exception ex) {
             validateException(Transaction.Exceptions.transactionMustBeOpenToReadWrite(), ex);
         }
@@ -30,7 +41,7 @@ public class TestRollback extends BaseTest {
         this.sqlgGraph.tx().commit();
         v1.property("name", "john");
         this.sqlgGraph.tx().rollback();
-        Assert.assertFalse(v1.property("name").isPresent());
+        assertFalse(v1.property("name").isPresent());
     }
 
     public static void validateException(final Throwable expected, final Throwable actual) {
