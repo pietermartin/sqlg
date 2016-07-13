4aa125941864f06bd81d5862a6099bae139a784a
pieter <pieter.martin@gmail.com>
2016-07-13 18:52:50 +0200

    put the label invalidation back.

--- sqlg-core/src/main/java/org/umlg/sqlg/sql/parse/SchemaTableTree.java
+++ sqlg-core/src/main/java/org/umlg/sqlg/sql/parse/SchemaTableTree.java
@@ -1843,7 +1843,18 @@ public class SchemaTableTree {
     private boolean invalidateByHas(SchemaTableTree schemaTableTree) {
         for (HasContainer hasContainer : schemaTableTree.hasContainers) {
             if (!hasContainer.getKey().equals(TopologyStrategy.TOPOLOGY_SELECTION_WITHOUT) && !hasContainer.getKey().equals(TopologyStrategy.TOPOLOGY_SELECTION_FROM)) {
-                if (!hasContainer.getKey().equals(T.id.getAccessor())) {
+                if (hasContainer.getKey().equals(label.getAccessor())) {
+                    //Check if we are on a vertex or edge
+                    SchemaTable hasContainerLabelSchemaTable;
+                    if (schemaTableTree.getSchemaTable().getTable().startsWith(SchemaManager.VERTEX_PREFIX)) {
+                        hasContainerLabelSchemaTable = SchemaTable.from(this.sqlgGraph, SchemaManager.VERTEX_PREFIX + hasContainer.getValue().toString(), this.sqlgGraph.getSqlDialect().getPublicSchema());
+                    } else {
+                        hasContainerLabelSchemaTable = SchemaTable.from(this.sqlgGraph, SchemaManager.EDGE_PREFIX + hasContainer.getValue().toString(), this.sqlgGraph.getSqlDialect().getPublicSchema());
+                    }
+                    if (hasContainer.getBiPredicate().equals(Compare.eq) && !hasContainerLabelSchemaTable.toString().equals(schemaTableTree.getSchemaTable().toString())) {
+                        return true;
+                    }
+                } else if (!hasContainer.getKey().equals(T.id.getAccessor())) {
                     //check if the hasContainer is for a property that exists, if not remove this node from the query tree
                     if (!this.getFilteredAllTables().get(schemaTableTree.getSchemaTable().toString()).containsKey(hasContainer.getKey())) {
                         return true;
