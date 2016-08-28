764c4f20993fb5689ca07ce99274e9dfcf7d647d
pieter <pieter@apache.org>
2016-08-28 16:35:37 +0200

    upgrade TinkerPop io to v2 style, this is not backwards compatible with v1.

--- sqlg-test/src/main/java/org/umlg/sqlg/AnyTest.java
+++ sqlg-test/src/main/java/org/umlg/sqlg/AnyTest.java
@@ -2,7 +2,7 @@ package org.umlg.sqlg;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.umlg.sqlg.test.vertexstep.localvertexstep.TestSchemaTableTreeAndHasContainer;
+import org.umlg.sqlg.test.topology.TestTopologyUpgrade;
 
 /**
  * Date: 2014/07/16
@@ -10,7 +10,7 @@ import org.umlg.sqlg.test.vertexstep.localvertexstep.TestSchemaTableTreeAndHasCo
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        TestSchemaTableTreeAndHasContainer.class
+        TestTopologyUpgrade.class
 })
 public class AnyTest {
 }
