19c6dfaec82b427ca52f50b3eda99dbfd2b0fd3b
pieter <pieter@apache.org>
2016-10-23 16:21:35 +0200

    add failing test. still working on why.

--- sqlg-test/src/main/java/org/umlg/sqlg/AnyTest.java
+++ sqlg-test/src/main/java/org/umlg/sqlg/AnyTest.java
@@ -2,7 +2,7 @@ package org.umlg.sqlg;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.umlg.sqlg.test.gremlincompile.TestGremlinCompileV;
+import org.umlg.sqlg.test.dedup.TestRepeatBoth;
 
 /**
  * Date: 2014/07/16
@@ -10,7 +10,7 @@ import org.umlg.sqlg.test.gremlincompile.TestGremlinCompileV;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        TestGremlinCompileV.class
+        TestRepeatBoth.class
 })
 public class AnyTest {
 }
