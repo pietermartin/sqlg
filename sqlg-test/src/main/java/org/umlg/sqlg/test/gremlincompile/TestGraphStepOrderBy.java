96caa860aa82b47998121a47dd1e79443077c76b
pieter <pieter@apache.org>
2016-10-25 05:45:49 +0200

    Comment tests back in. No change

--- sqlg-test/src/main/java/org/umlg/sqlg/test/gremlincompile/TestGraphStepOrderBy.java
+++ sqlg-test/src/main/java/org/umlg/sqlg/test/gremlincompile/TestGraphStepOrderBy.java
@@ -17,29 +17,29 @@ import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.select
  */
 public class TestGraphStepOrderBy extends BaseTest {
 
-//    @Test
-//    public void testOrderBy() {
-//        Vertex a1 = this.sqlgGraph.addVertex(T.label, "A", "name", "a", "surname", "a");
-//        Vertex a2 = this.sqlgGraph.addVertex(T.label, "A", "name", "a", "surname", "b");
-//        Vertex a3 = this.sqlgGraph.addVertex(T.label, "A", "name", "a", "surname", "c");
-//        Vertex b1 = this.sqlgGraph.addVertex(T.label, "A", "name", "b", "surname", "a");
-//        Vertex b2 = this.sqlgGraph.addVertex(T.label, "A", "name", "b", "surname", "b");
-//        Vertex b3 = this.sqlgGraph.addVertex(T.label, "A", "name", "b", "surname", "c");
-//        this.sqlgGraph.tx().commit();
-//
-//        List<Vertex> result = this.sqlgGraph.traversal().V().hasLabel("A")
-//                .order()
-//                .by("name", Order.incr).by("surname", Order.decr)
-//                .toList();
-//
-//        Assert.assertEquals(6, result.size());
-//        Assert.assertEquals(a3, result.get(0));
-//        Assert.assertEquals(a2, result.get(1));
-//        Assert.assertEquals(a1, result.get(2));
-//        Assert.assertEquals(b3, result.get(3));
-//        Assert.assertEquals(b2, result.get(4));
-//        Assert.assertEquals(b1, result.get(5));
-//    }
+    @Test
+    public void testOrderBy() {
+        Vertex a1 = this.sqlgGraph.addVertex(T.label, "A", "name", "a", "surname", "a");
+        Vertex a2 = this.sqlgGraph.addVertex(T.label, "A", "name", "a", "surname", "b");
+        Vertex a3 = this.sqlgGraph.addVertex(T.label, "A", "name", "a", "surname", "c");
+        Vertex b1 = this.sqlgGraph.addVertex(T.label, "A", "name", "b", "surname", "a");
+        Vertex b2 = this.sqlgGraph.addVertex(T.label, "A", "name", "b", "surname", "b");
+        Vertex b3 = this.sqlgGraph.addVertex(T.label, "A", "name", "b", "surname", "c");
+        this.sqlgGraph.tx().commit();
+
+        List<Vertex> result = this.sqlgGraph.traversal().V().hasLabel("A")
+                .order()
+                .by("name", Order.incr).by("surname", Order.decr)
+                .toList();
+
+        Assert.assertEquals(6, result.size());
+        Assert.assertEquals(a3, result.get(0));
+        Assert.assertEquals(a2, result.get(1));
+        Assert.assertEquals(a1, result.get(2));
+        Assert.assertEquals(b3, result.get(3));
+        Assert.assertEquals(b2, result.get(4));
+        Assert.assertEquals(b1, result.get(5));
+    }
 
     @Test
     public void testOrderBy2() {
@@ -120,43 +120,43 @@ public class TestGraphStepOrderBy extends BaseTest {
         Assert.assertEquals("RNC", row8.get("nng").value("name"));
         Assert.assertEquals("RNCA", row8.get("nn").value("name"));
     }
-//
-//    @Test
-//    public void testOrderby3() {
-//        Vertex a1 = this.sqlgGraph.addVertex(T.label, "A", "name", "aa");
-//        Vertex a2 = this.sqlgGraph.addVertex(T.label, "A", "name", "ab");
-//        Vertex b1 = this.sqlgGraph.addVertex(T.label, "B", "name", "ba");
-//        Vertex b2 = this.sqlgGraph.addVertex(T.label, "B", "name", "bb");
-//        Vertex b3 = this.sqlgGraph.addVertex(T.label, "B", "name", "bc");
-//        Vertex b4 = this.sqlgGraph.addVertex(T.label, "B", "name", "bd");
-//        a1.addEdge("ab", b1);
-//        a1.addEdge("ab", b2);
-//        a2.addEdge("ab", b3);
-//        a2.addEdge("ab", b4);
-//        this.sqlgGraph.tx().commit();
-//
-//        List<Map<String, Vertex>> result = this.sqlgGraph.traversal().V()
-//                .hasLabel("A").as("a")
-//                .out("ab").as("b")
-//                .<Vertex>select("a", "b")
-//                .order()
-//                .by(select("a").by("name"), Order.incr)
-//                .by(select("b").by("name"), Order.decr)
-//                .toList();
-//
-//        Assert.assertEquals(4, result.size());
-//        Map<String, Vertex> map1 = result.get(0);
-//        Map<String, Vertex> map2 = result.get(1);
-//        Map<String, Vertex> map3 = result.get(2);
-//        Map<String, Vertex> map4 = result.get(3);
-//        Assert.assertEquals("aa", map1.get("a").value("name"));
-//        Assert.assertEquals("bb", map1.get("b").value("name"));
-//        Assert.assertEquals("aa", map2.get("a").value("name"));
-//        Assert.assertEquals("ba", map2.get("b").value("name"));
-//        Assert.assertEquals("ab", map3.get("a").value("name"));
-//        Assert.assertEquals("bd", map3.get("b").value("name"));
-//        Assert.assertEquals("ab", map4.get("a").value("name"));
-//        Assert.assertEquals("bc", map4.get("b").value("name"));
-//    }
+
+    @Test
+    public void testOrderby3() {
+        Vertex a1 = this.sqlgGraph.addVertex(T.label, "A", "name", "aa");
+        Vertex a2 = this.sqlgGraph.addVertex(T.label, "A", "name", "ab");
+        Vertex b1 = this.sqlgGraph.addVertex(T.label, "B", "name", "ba");
+        Vertex b2 = this.sqlgGraph.addVertex(T.label, "B", "name", "bb");
+        Vertex b3 = this.sqlgGraph.addVertex(T.label, "B", "name", "bc");
+        Vertex b4 = this.sqlgGraph.addVertex(T.label, "B", "name", "bd");
+        a1.addEdge("ab", b1);
+        a1.addEdge("ab", b2);
+        a2.addEdge("ab", b3);
+        a2.addEdge("ab", b4);
+        this.sqlgGraph.tx().commit();
+
+        List<Map<String, Vertex>> result = this.sqlgGraph.traversal().V()
+                .hasLabel("A").as("a")
+                .out("ab").as("b")
+                .<Vertex>select("a", "b")
+                .order()
+                .by(select("a").by("name"), Order.incr)
+                .by(select("b").by("name"), Order.decr)
+                .toList();
+
+        Assert.assertEquals(4, result.size());
+        Map<String, Vertex> map1 = result.get(0);
+        Map<String, Vertex> map2 = result.get(1);
+        Map<String, Vertex> map3 = result.get(2);
+        Map<String, Vertex> map4 = result.get(3);
+        Assert.assertEquals("aa", map1.get("a").value("name"));
+        Assert.assertEquals("bb", map1.get("b").value("name"));
+        Assert.assertEquals("aa", map2.get("a").value("name"));
+        Assert.assertEquals("ba", map2.get("b").value("name"));
+        Assert.assertEquals("ab", map3.get("a").value("name"));
+        Assert.assertEquals("bd", map3.get("b").value("name"));
+        Assert.assertEquals("ab", map4.get("a").value("name"));
+        Assert.assertEquals("bc", map4.get("b").value("name"));
+    }
 
 }
