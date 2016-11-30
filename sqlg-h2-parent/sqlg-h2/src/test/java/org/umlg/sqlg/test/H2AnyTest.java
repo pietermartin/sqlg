d553869446d885fdcd1e2c3f3b99ffd60eba8053
pieter <pieter@apache.org>
2016-11-30 14:54:23 +0200

    the test to run annotation is on AnyTest

--- sqlg-h2-parent/sqlg-h2/src/test/java/org/umlg/sqlg/test/H2AnyTest.java
+++ sqlg-h2-parent/sqlg-h2/src/test/java/org/umlg/sqlg/test/H2AnyTest.java
@@ -1,17 +1,10 @@
 package org.umlg.sqlg.test;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 import org.umlg.sqlg.AnyTest;
-import org.umlg.sqlg.test.schema.TestLoadSchema;
 
 /**
  * @author Lukas Krejci
  * @since 1.3.0
  */
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
-         TestLoadSchema.class
-})
 public class H2AnyTest extends AnyTest {
 }
