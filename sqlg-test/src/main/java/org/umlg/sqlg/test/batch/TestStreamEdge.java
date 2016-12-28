55c41130821e31408f2a75ea5863b90300667b99
pieter <pieter@apache.org>
2016-12-28 13:18:37 +0200

    snap keep OutOfMemory

--- sqlg-test/src/main/java/org/umlg/sqlg/test/batch/TestStreamEdge.java
+++ sqlg-test/src/main/java/org/umlg/sqlg/test/batch/TestStreamEdge.java
@@ -145,6 +145,25 @@ public class TestStreamEdge extends BaseTest {
             person.streamEdge("person_car", car, keyValues);
         }
         this.sqlgGraph.tx().commit();
+        int mb = 1024 * 1024;
+
+        // get Runtime instance
+        Runtime instance = Runtime.getRuntime();
+
+        System.out.println("***** Heap utilization statistics [MB] *****\n");
+
+        // available memory
+        System.out.println("Total Memory: " + instance.totalMemory() / mb);
+
+        // free memory
+        System.out.println("Free Memory: " + instance.freeMemory() / mb);
+
+        // used memory
+        System.out.println("Used Memory: "
+                + (instance.totalMemory() - instance.freeMemory()) / mb);
+
+        // Maximum available memory
+        System.out.println("Max Memory: " + instance.maxMemory() / mb);
         Assert.assertEquals(NUMBER_OF_VERTICES, this.sqlgGraph.traversal().V(persons.get(0)).out("person_car").toList().size());
         stopWatch.stop();
         System.out.println("testMilCompleteEdges took " + stopWatch.toString());
