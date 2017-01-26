e6871e2475d21a659dba50a6d0c3e60a4a3038b3
pieter <pieter@apache.org>
2017-01-26 15:48:01 +0200

    added assume batch

--- sqlg-test/src/main/java/org/umlg/sqlg/test/batch/TestBatchNormalUpdateDateTimeArrays.java
+++ sqlg-test/src/main/java/org/umlg/sqlg/test/batch/TestBatchNormalUpdateDateTimeArrays.java
@@ -3,6 +3,8 @@ package org.umlg.sqlg.test.batch;
 import org.apache.tinkerpop.gremlin.structure.T;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
 import org.junit.Test;
 import org.umlg.sqlg.test.BaseTest;
 
@@ -15,6 +17,11 @@ import java.util.List;
  * Time: 12:22 PM
  */
 public class TestBatchNormalUpdateDateTimeArrays extends BaseTest {
+    
+    @Before
+    public void beforeTest() {
+        Assume.assumeTrue(this.sqlgGraph.getSqlDialect().supportsBatchMode());
+    }
 
     @Test
     public void testUpdateLocalDateTimeArray() {
