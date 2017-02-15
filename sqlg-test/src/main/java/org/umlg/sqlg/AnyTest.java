5378aeba4867a456f791631b0160645e4b0bd5cd
pieter <pieter@apache.org>
2017-02-15 20:56:10 +0200

    change test to run

--- sqlg-test/src/main/java/org/umlg/sqlg/AnyTest.java
+++ sqlg-test/src/main/java/org/umlg/sqlg/AnyTest.java
@@ -2,7 +2,7 @@ package org.umlg.sqlg;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.umlg.sqlg.test.gremlincompile.TestGremlinCompileWithHas;
+import org.umlg.sqlg.test.gremlincompile.TestOptionalWithOrder;
 
 /**
  * Date: 2014/07/16
@@ -10,7 +10,7 @@ import org.umlg.sqlg.test.gremlincompile.TestGremlinCompileWithHas;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        TestGremlinCompileWithHas.class
+        TestOptionalWithOrder.class
 })
 public class AnyTest {
 }
