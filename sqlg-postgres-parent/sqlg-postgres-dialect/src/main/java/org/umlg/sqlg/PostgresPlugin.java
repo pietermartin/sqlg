3d0ad8c7e92717da8bece6dfc4339f0cc67fa987
pieter <pieter.martin@riseup.net>
2017-09-04 18:23:53 +0200

    remove commented out code

--- sqlg-postgres-parent/sqlg-postgres-dialect/src/main/java/org/umlg/sqlg/PostgresPlugin.java
+++ sqlg-postgres-parent/sqlg-postgres-dialect/src/main/java/org/umlg/sqlg/PostgresPlugin.java
@@ -19,7 +19,6 @@ public class PostgresPlugin implements SqlgPlugin {
 
     @Override
     public String getDriverFor(String connectionUrl) {
-//        return connectionUrl.startsWith("jdbc:postgresql") ? "org.postgresql.xa.PGXADataSource" : null;
         return connectionUrl.startsWith("jdbc:postgresql") ? "org.postgresql.Driver" : null;
     }
 
