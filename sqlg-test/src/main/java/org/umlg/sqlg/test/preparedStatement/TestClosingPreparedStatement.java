d43e181b1f66bd97dedf64eea8748817bfd13827
Jean-Philippe Moresmau <jean-philippe.moresmau@asg.com>
2018-06-07 22:39:30 +0200

    Remove closed statements from cache

--- sqlg-test/src/main/java/org/umlg/sqlg/test/preparedStatement/TestClosingPreparedStatement.java
+++ sqlg-test/src/main/java/org/umlg/sqlg/test/preparedStatement/TestClosingPreparedStatement.java
@@ -1,7 +1,14 @@
 package org.umlg.sqlg.test.preparedStatement;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import org.apache.commons.lang3.time.StopWatch;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.structure.T;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.junit.Test;
 import org.umlg.sqlg.test.BaseTest;
 
@@ -25,10 +32,10 @@ public class TestClosingPreparedStatement extends BaseTest {
 //    }
 
     @Test
-    public void test() {
+    public void testAddCommit() {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
-        for (int i = 0; i < 1_000_000; i++) {
+        for (int i = 0; i < 100_000; i++) {
             this.sqlgGraph.addVertex(T.label, "A");
             if (i % 10_000 == 0) {
                 System.out.println(i);
@@ -36,10 +43,35 @@ public class TestClosingPreparedStatement extends BaseTest {
             }
             if (i % 1000 == 0) {
                 this.sqlgGraph.tx().commit();
+                assertTrue(this.sqlgGraph.tx().getPreparedStatementCache().isEmpty());
             }
         }
         this.sqlgGraph.tx().commit();
         stopWatch.stop();
         System.out.println(stopWatch.toString());
+    }
+    
+    @Test
+    public void testRead() {
+    	 Vertex v=this.sqlgGraph.addVertex(T.label, "A");
+    	 this.sqlgGraph.tx().commit();
+	     GraphTraversal<Vertex, Vertex> gt = sqlgGraph.traversal().V(v.id());
+         try {
+        	 Vertex v2= gt.next();
+        	 assertEquals(v.id(),v2.id());
+        	 assertFalse(this.sqlgGraph.tx().getPreparedStatementCache().isEmpty());
+        	 assertFalse(gt.hasNext());
+        	 assertTrue(this.sqlgGraph.tx().getPreparedStatementCache().isEmpty());
+         } finally {
+               try {
+                     gt.close(); 
+                     gt = null;
+               } catch (Exception e) {
+            	   e.printStackTrace();
+            	   fail(e.getMessage());
+               }
+         }
+
+    	 
     }
 }
