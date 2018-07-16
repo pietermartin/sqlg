43ef996ea5d9979d3daabae9e01e443da6173c66
pieter <pieter.martin@digitata.com>
2018-07-16 16:49:29 +0200

    cosmetic

--- sqlg-test/src/main/java/org/umlg/sqlg/test/TestAddVertexViaMap.java
+++ sqlg-test/src/main/java/org/umlg/sqlg/test/TestAddVertexViaMap.java
@@ -10,10 +10,7 @@ import org.umlg.sqlg.structure.PropertyType;
 import org.umlg.sqlg.structure.SqlgVertex;
 import org.umlg.sqlg.structure.topology.VertexLabel;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * Date: 2014/07/22
@@ -21,34 +18,34 @@ import java.util.UUID;
  */
 public class TestAddVertexViaMap extends BaseTest {
 
+//    @Test
+//    public void testMap() {
+//        Map<String, Object> map = new HashMap<>();
+//        map.put("name1", "p1");
+//        map.put("name2", "p2");
+//        map.put("name3", "p3");
+//        Vertex v1 = this.sqlgGraph.addVertex("Person", map);
+//        this.sqlgGraph.tx().commit();
+//        Vertex v2 = this.sqlgGraph.traversal().V().<Vertex>has(T.label, "Person").next();
+//        Assert.assertEquals(v1, v2);
+//        Assert.assertEquals("p1", v2.property("name1").value());
+//        Assert.assertEquals("p2", v2.property("name2").value());
+//        Assert.assertEquals("p3", v2.property("name3").value());
+//
+//
+//        Map<String, Object> edgeMap = new HashMap<>();
+//        edgeMap.put("name1", "p1");
+//        edgeMap.put("name2", "p2");
+//        edgeMap.put("name3", "p3");
+//        Edge e1 = ((SqlgVertex) v1).addEdgeWithMap("e1", v2, edgeMap);
+//        this.sqlgGraph.tx().commit();
+//        Assert.assertEquals("p1", e1.property("name1").value());
+//        Assert.assertEquals("p2", e1.property("name2").value());
+//        Assert.assertEquals("p3", e1.property("name3").value());
+//    }
+
     @Test
-    public void testMap() {
-        Map<String, Object> map = new HashMap<>();
-        map.put("name1", "p1");
-        map.put("name2", "p2");
-        map.put("name3", "p3");
-        Vertex v1 = this.sqlgGraph.addVertex("Person", map);
-        this.sqlgGraph.tx().commit();
-        Vertex v2 = this.sqlgGraph.traversal().V().<Vertex>has(T.label, "Person").next();
-        Assert.assertEquals(v1, v2);
-        Assert.assertEquals("p1", v2.property("name1").value());
-        Assert.assertEquals("p2", v2.property("name2").value());
-        Assert.assertEquals("p3", v2.property("name3").value());
-
-
-        Map<String, Object> edgeMap = new HashMap<>();
-        edgeMap.put("name1", "p1");
-        edgeMap.put("name2", "p2");
-        edgeMap.put("name3", "p3");
-        Edge e1 = ((SqlgVertex) v1).addEdgeWithMap("e1", v2, edgeMap);
-        this.sqlgGraph.tx().commit();
-        Assert.assertEquals("p1", e1.property("name1").value());
-        Assert.assertEquals("p2", e1.property("name2").value());
-        Assert.assertEquals("p3", e1.property("name3").value());
-    }
-
-    @Test
-    public void testMapUsserSuppliedPK() {
+    public void testMapUserSuppliedPK() {
         VertexLabel vertexLabel = this.sqlgGraph.getTopology().getPublicSchema()
                 .ensureVertexLabelExist(
                         "Person",
@@ -58,7 +55,7 @@ public class TestAddVertexViaMap extends BaseTest {
                             put("name2", PropertyType.STRING);
                             put("name3", PropertyType.STRING);
                         }},
-                        ListOrderedSet.listOrderedSet(Arrays.asList("uid"))
+                        ListOrderedSet.listOrderedSet(Collections.singletonList("uid"))
                 );
         Map<String, Object> map = new HashMap<>();
         map.put("uid", UUID.randomUUID().toString());
@@ -83,7 +80,7 @@ public class TestAddVertexViaMap extends BaseTest {
                     put("name2", PropertyType.STRING);
                     put("name3", PropertyType.STRING);
                 }},
-                ListOrderedSet.listOrderedSet(Arrays.asList("uid"))
+                ListOrderedSet.listOrderedSet(Collections.singletonList("uid"))
         );
 
         Map<String, Object> edgeMap = new HashMap<>();
@@ -98,19 +95,5 @@ public class TestAddVertexViaMap extends BaseTest {
         Assert.assertEquals("p3", e1.property("name3").value());
     }
 
-//    @Test
-//    public void howToUpdateManyRows() {
-//        this.sqlgGraph.addVertex(T.label, "Person", "name", "marko");
-//        this.sqlgGraph.addVertex(T.label, "Person", "name", "joe");
-//        this.sqlgGraph.addVertex(T.label, "Person", "name", "john");
-//        this.sqlgGraph.addVertex(T.label, "Person", "name", "peter");
-//        this.sqlgGraph.tx().commit();
-//
-//        List<Vertex> markos = this.sqlgGraph.traversal().V().has(T.label, "Person").<Vertex>has("name", "marko").toList();
-//        Assert.assertFalse(markos.isEmpty());
-//        Assert.assertEquals(1, markos.size());
-//        markos.get(0).property("name", "marko2");
-//        this.sqlgGraph.tx().commit();
-//    }
 
 }
