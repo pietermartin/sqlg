ed93548d78b0e0f690ea4db4db3cc8ecbdd9d642
pieter <pieter.martin@digitata.com>
2018-08-02 13:25:43 +0200

    working on getting teamcity to pass

--- sqlg-test/src/main/java/org/umlg/sqlg/AnyTest.java
+++ sqlg-test/src/main/java/org/umlg/sqlg/AnyTest.java
@@ -2,7 +2,7 @@ package org.umlg.sqlg;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.umlg.sqlg.test.batch.TestBatchNormalUpdate;
+import org.umlg.sqlg.test.roles.TestReadOnlyRole;
 
 /**
  * Date: 2014/07/16
@@ -10,7 +10,7 @@ import org.umlg.sqlg.test.batch.TestBatchNormalUpdate;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        TestBatchNormalUpdate.class
+        TestReadOnlyRole.class
 })
 public class AnyTest {
 }
