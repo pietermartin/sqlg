c860925ae856eceaf8b62ed148434a2fc2144266
pieter <pieter.martin@digitata.com>
2018-08-26 21:30:17 +0200

    need a custom AddV step as it needs to create a SqlgTraverser via the generator

---
+++ sqlg-core/src/main/java/org/umlg/sqlg/step/SqlgAddVertexStartStep.java
@@ -0,0 +1,113 @@
+package org.umlg.sqlg.step;
+
+import org.apache.tinkerpop.gremlin.process.traversal.*;
+import org.apache.tinkerpop.gremlin.process.traversal.step.Mutating;
+import org.apache.tinkerpop.gremlin.process.traversal.step.Scoping;
+import org.apache.tinkerpop.gremlin.process.traversal.step.TraversalParent;
+import org.apache.tinkerpop.gremlin.process.traversal.step.util.Parameters;
+import org.apache.tinkerpop.gremlin.process.traversal.step.util.event.CallbackRegistry;
+import org.apache.tinkerpop.gremlin.process.traversal.step.util.event.Event;
+import org.apache.tinkerpop.gremlin.process.traversal.step.util.event.ListCallbackRegistry;
+import org.apache.tinkerpop.gremlin.process.traversal.strategy.decoration.EventStrategy;
+import org.apache.tinkerpop.gremlin.process.traversal.traverser.TraverserRequirement;
+import org.apache.tinkerpop.gremlin.process.traversal.util.FastNoSuchElementException;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.apache.tinkerpop.gremlin.structure.util.StringFactory;
+import org.umlg.sqlg.structure.SqlgTraverserGenerator;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Pieter Martin (https://github.com/pietermartin)
+ * Date: 2018/08/26
+ */
+public class SqlgAddVertexStartStep extends SqlgAbstractStep<Vertex, Vertex> implements Mutating<Event.VertexAddedEvent>, TraversalParent, Parameterizing, Scoping {
+
+    private Parameters parameters = new Parameters();
+    private boolean first = true;
+    private CallbackRegistry<Event.VertexAddedEvent> callbackRegistry;
+
+    public SqlgAddVertexStartStep(final Traversal.Admin traversal, Parameters parameters) {
+        super(traversal);
+        this.parameters = parameters;
+    }
+
+    @Override
+    public Parameters getParameters() {
+        return this.parameters;
+    }
+
+    @Override
+    public Set<String> getScopeKeys() {
+        return this.parameters.getReferencedLabels();
+    }
+
+    @Override
+    public <S, E> List<Traversal.Admin<S, E>> getLocalChildren() {
+        return this.parameters.getTraversals();
+    }
+
+    @Override
+    public void addPropertyMutations(final Object... keyValues) {
+        this.parameters.set(this, keyValues);
+    }
+
+    @Override
+    protected Traverser.Admin<Vertex> processNextStart() {
+        if (this.first) {
+            this.first = false;
+            final SqlgTraverserGenerator generator = SqlgTraverserGenerator.instance();
+            final Vertex vertex = this.getTraversal().getGraph().get().addVertex(
+                    this.parameters.getKeyValues(
+                            generator.generate(false, (Step)this, 1L, false, false)
+                    ));
+            if (this.callbackRegistry != null && !this.callbackRegistry.getCallbacks().isEmpty()) {
+                final EventStrategy eventStrategy = getTraversal().getStrategies().getStrategy(EventStrategy.class).get();
+                final Event.VertexAddedEvent vae = new Event.VertexAddedEvent(eventStrategy.detach(vertex));
+                this.callbackRegistry.getCallbacks().forEach(c -> c.accept(vae));
+            }
+            return generator.generate(vertex, (Step)this, 1L, false, false);
+        } else
+            throw FastNoSuchElementException.instance();
+    }
+
+    @Override
+    public CallbackRegistry<Event.VertexAddedEvent> getMutatingCallbackRegistry() {
+        if (null == this.callbackRegistry) this.callbackRegistry = new ListCallbackRegistry<>();
+        return this.callbackRegistry;
+    }
+
+    @Override
+    public Set<TraverserRequirement> getRequirements() {
+        return this.getSelfAndChildRequirements();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode() ^ this.parameters.hashCode();
+    }
+
+    @Override
+    public void reset() {
+        super.reset();
+    }
+
+    @Override
+    public String toString() {
+        return StringFactory.stepString(this, this.parameters);
+    }
+
+    @Override
+    public void setTraversal(final Traversal.Admin<?, ?> parentTraversal) {
+        super.setTraversal(parentTraversal);
+        this.parameters.getTraversals().forEach(this::integrateChild);
+    }
+
+    @Override
+    public SqlgAddVertexStartStep clone() {
+        final SqlgAddVertexStartStep clone = (SqlgAddVertexStartStep) super.clone();
+        clone.parameters = this.parameters.clone();
+        return clone;
+    }
+}
---
+++ sqlg-test/src/main/java/org/umlg/sqlg/test/mod/TestTraversalAddV.java
@@ -0,0 +1,135 @@
+package org.umlg.sqlg.test.mod;
+
+import org.apache.tinkerpop.gremlin.process.traversal.Traversal;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
+import org.apache.tinkerpop.gremlin.process.traversal.step.util.event.MutationListener;
+import org.apache.tinkerpop.gremlin.process.traversal.strategy.decoration.EventStrategy;
+import org.apache.tinkerpop.gremlin.structure.Edge;
+import org.apache.tinkerpop.gremlin.structure.Property;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.apache.tinkerpop.gremlin.structure.VertexProperty;
+import org.apache.tinkerpop.gremlin.structure.util.detached.DetachedVertex;
+import org.hamcrest.core.Is;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Assert;
+import org.junit.Test;
+import org.umlg.sqlg.test.BaseTest;
+
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author Pieter Martin (https://github.com/pietermartin)
+ * Date: 2018/08/26
+ */
+public class TestTraversalAddV extends BaseTest {
+
+    @Test
+    public void g_addVXV_hasXname_markoX_propertiesXnameX_keyX_label() {
+        loadModern();
+        final Traversal<Vertex, String> traversal =  this.sqlgGraph.traversal().addV(__.V().has("name", "marko").properties("name").key()).label();
+        printTraversalForm(traversal);
+        Assert.assertEquals("name", traversal.next());
+        Assert.assertFalse(traversal.hasNext());
+    }
+
+    @Test
+    public void g_addV_asXfirstX_repeatXaddEXnextX_toXaddVX_inVX_timesX5X_addEXnextX_toXselectXfirstXX() {
+        final Traversal<Vertex, Edge> traversal = this.sqlgGraph.traversal()
+                .addV().as("first")
+                .repeat(
+                        __.addE("next").to(__.addV()).inV()).times(5)
+                .addE("next")
+                .to(__.select("first"));
+        printTraversalForm(traversal);
+        Assert.assertEquals("next", traversal.next().label());
+        Assert.assertFalse(traversal.hasNext());
+        Assert.assertEquals(6L, this.sqlgGraph.traversal().V().count().next().longValue());
+        Assert.assertEquals(6L, this.sqlgGraph.traversal().E().count().next().longValue());
+        Assert.assertEquals(Arrays.asList(2L, 2L, 2L, 2L, 2L, 2L), this.sqlgGraph.traversal().V().map(__.bothE().count()).toList());
+        Assert.assertEquals(Arrays.asList(1L, 1L, 1L, 1L, 1L, 1L), this.sqlgGraph.traversal().V().map(__.inE().count()).toList());
+        Assert.assertEquals(Arrays.asList(1L, 1L, 1L, 1L, 1L, 1L), this.sqlgGraph.traversal().V().map(__.outE().count()).toList());
+    }
+
+    @Test
+    public void shouldDetachVertexWhenAdded() {
+        final AtomicBoolean triggered = new AtomicBoolean(false);
+
+        final MutationListener listener = new AbstractMutationListener() {
+            @Override
+            public void vertexAdded(final Vertex element) {
+                Assert.assertThat(element, IsInstanceOf.instanceOf(DetachedVertex.class));
+                Assert.assertEquals("thing", element.label());
+                Assert.assertEquals("there", element.value("here"));
+                triggered.set(true);
+            }
+        };
+        final EventStrategy.Builder builder = EventStrategy.build().addListener(listener);
+
+        builder.eventQueue(new EventStrategy.TransactionalEventQueue(this.sqlgGraph));
+
+        final EventStrategy eventStrategy = builder.create();
+        final GraphTraversalSource gts = create(eventStrategy);
+
+        gts.addV("thing").property("here", "there").iterate();
+        sqlgGraph.tx().commit();
+        Assert.assertThat(triggered.get(), Is.is(true));
+    }
+
+    private GraphTraversalSource create(final EventStrategy strategy) {
+        return this.sqlgGraph.traversal().withStrategies(strategy);
+    }
+
+    static abstract class AbstractMutationListener implements MutationListener {
+        @Override
+        public void vertexAdded(final Vertex vertex) {
+
+        }
+
+        @Override
+        public void vertexRemoved(final Vertex vertex) {
+
+        }
+
+        @Override
+        public void vertexPropertyChanged(final Vertex element, final Property oldValue, final Object setValue, final Object... vertexPropertyKeyValues) {
+
+        }
+
+        @Override
+        public void vertexPropertyRemoved(final VertexProperty vertexProperty) {
+
+        }
+
+        @Override
+        public void edgeAdded(final Edge edge) {
+
+        }
+
+        @Override
+        public void edgeRemoved(final Edge edge) {
+
+        }
+
+        @Override
+        public void edgePropertyChanged(final Edge element, final Property oldValue, final Object setValue) {
+
+        }
+
+        @Override
+        public void edgePropertyRemoved(final Edge element, final Property property) {
+
+        }
+
+        @Override
+        public void vertexPropertyPropertyChanged(final VertexProperty element, final Property oldValue, final Object setValue) {
+
+        }
+
+        @Override
+        public void vertexPropertyPropertyRemoved(final VertexProperty element, final Property property) {
+
+        }
+    }
+}
