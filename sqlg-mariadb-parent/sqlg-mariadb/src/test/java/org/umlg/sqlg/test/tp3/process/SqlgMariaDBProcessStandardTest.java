7de60f1aa904dd90b174e7f541bb21cfe90b55a9
Pieter Martin <pieter.martin@digitata.com>
2021-04-10 20:54:12 +0200

    TinkerPop process test

--- sqlg-mariadb-parent/sqlg-mariadb/src/test/java/org/umlg/sqlg/test/tp3/process/SqlgMariaDBProcessStandardTest.java
+++ sqlg-mariadb-parent/sqlg-mariadb/src/test/java/org/umlg/sqlg/test/tp3/process/SqlgMariaDBProcessStandardTest.java
@@ -1,7 +1,7 @@
-package org.umlg.sqlg.test.tp3.structure;
+package org.umlg.sqlg.test.tp3.process;
 
 import org.apache.tinkerpop.gremlin.GraphProviderClass;
-import org.apache.tinkerpop.gremlin.structure.StructureStandardSuite;
+import org.apache.tinkerpop.gremlin.process.ProcessStandardSuite;
 import org.junit.runner.RunWith;
 import org.umlg.sqlg.structure.SqlgGraph;
 import org.umlg.sqlg.test.tp3.SqlgMariaDBProvider;
@@ -10,7 +10,7 @@ import org.umlg.sqlg.test.tp3.SqlgMariaDBProvider;
 /**
  * Executes the Standard Gremlin Structure Test Suite using SqlG.
  */
-@RunWith(StructureStandardSuite.class)
+@RunWith(ProcessStandardSuite.class)
 @GraphProviderClass(provider = SqlgMariaDBProvider.class, graph = SqlgGraph.class)
-public class SqlgMariaDBStructureStandardTest {
+public class SqlgMariaDBProcessStandardTest {
 }
