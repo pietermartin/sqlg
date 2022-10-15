d264c52fcfae9785aaa09324cd04578d1e87d721
pieter <pieter.martin@riseup.net>
2022-10-15 08:07:08 +0200

    cosmetic

--- sqlg-core/src/main/java/org/umlg/sqlg/predicate/Existence.java
+++ sqlg-core/src/main/java/org/umlg/sqlg/predicate/Existence.java
@@ -4,28 +4,30 @@ import java.util.function.BiPredicate;
 
 /**
  * abuse BiPredicate to represent nulls
- * @author JP Moresmau
  *
+ * @author JP Moresmau
  */
 public enum Existence implements BiPredicate<String, String> {
-	NULL {
-		@Override
-		public boolean test(String t, String u) {
-			return t==null || t.length()==0;
-		}
-		@Override
-		public String toString() {
-			return "IS NULL";
-		}
-	},
-	NOTNULL{
-		@Override
-		public boolean test(String t, String u) {
-			return t!=null && t.length()>0;
-		}
-		@Override
-		public String toString() {
-			return "IS NOT NULL";
-		}
-	}
+    NULL {
+        @Override
+        public boolean test(String t, String u) {
+            return t == null || t.length() == 0;
+        }
+
+        @Override
+        public String toString() {
+            return "IS NULL";
+        }
+    },
+    NOTNULL {
+        @Override
+        public boolean test(String t, String u) {
+            return t != null && t.length() > 0;
+        }
+
+        @Override
+        public String toString() {
+            return "IS NOT NULL";
+        }
+    }
 }
