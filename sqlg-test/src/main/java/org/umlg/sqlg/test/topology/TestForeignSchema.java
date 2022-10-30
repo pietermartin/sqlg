fde0165e96d3ab404bc0bdc102c1af0c7b69238f
pieter <pieter.martin@riseup.net>
2022-10-30 15:16:41 +0200

    cosmetic

--- sqlg-test/src/main/java/org/umlg/sqlg/test/topology/TestForeignSchema.java
+++ sqlg-test/src/main/java/org/umlg/sqlg/test/topology/TestForeignSchema.java
@@ -38,7 +38,7 @@ import java.util.concurrent.TimeUnit;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-@SuppressWarnings({"rawtypes", "DuplicatedCode"})
+@SuppressWarnings({"DuplicatedCode"})
 public class TestForeignSchema extends BaseTest {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(TestForeignSchema.class);
