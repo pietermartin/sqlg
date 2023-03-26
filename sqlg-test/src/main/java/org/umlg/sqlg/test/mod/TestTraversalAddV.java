53ffea435def4d65954b8eb916013514b671b721
pieter martin <pieter.martin@riseup.net>
2023-03-27 00:00:01 +0200

    added test from feature tests

--- sqlg-test/src/main/java/org/umlg/sqlg/test/mod/TestTraversalAddV.java
+++ sqlg-test/src/main/java/org/umlg/sqlg/test/mod/TestTraversalAddV.java
@@ -17,6 +17,7 @@ import org.junit.Test;
 import org.umlg.sqlg.test.BaseTest;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -24,6 +25,50 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * Date: 2018/08/26
  */
 public class TestTraversalAddV extends BaseTest {
+
+//    Scenario: g_V_addVXanimalX_propertyXage_0X
+//    Given the empty graph
+//    And the graph initializer of
+//      """
+//      g.addV("person").property("name", "marko").property("age", 29).as("marko").
+//        addV("person").property("name", "vadas").property("age", 27).as("vadas").
+//        addV("software").property("name", "lop").property("lang", "java").as("lop").
+//        addV("person").property("name","josh").property("age", 32).as("josh").
+//        addV("software").property("name", "ripple").property("lang", "java").as("ripple").
+//        addV("person").property("name", "peter").property("age", 35).as('peter').
+//        addE("knows").from("marko").to("vadas").property("weight", 0.5d).
+//        addE("knows").from("marko").to("josh").property("weight", 1.0d).
+//        addE("created").from("marko").to("lop").property("weight", 0.4d).
+//        addE("created").from("josh").to("ripple").property("weight", 1.0d).
+//        addE("created").from("josh").to("lop").property("weight", 0.4d).
+//        addE("created").from("peter").to("lop").property("weight", 0.2d)
+//      """
+//    And the traversal of
+//      """
+//      g.V().addV("animal").property("age", 0)
+//      """
+//    When iterated to list
+//    Then the result should have a count of 6
+//    And the graph should return 6 for count of "g.V().has(\"animal\",\"age\",0)"
+    @Test
+    public void g_V_addVXanimalX_propertyXage_0X() {
+      this.sqlgGraph.traversal().addV("person").property("name", "marko").property("age", 29).as("marko").
+        addV("person").property("name", "vadas").property("age", 27).as("vadas").
+        addV("software").property("name", "lop").property("lang", "java").as("lop").
+        addV("person").property("name","josh").property("age", 32).as("josh").
+        addV("software").property("name", "ripple").property("lang", "java").as("ripple").
+        addV("person").property("name", "peter").property("age", 35).as("peter").
+        addE("knows").from("marko").to("vadas").property("weight", 0.5d).
+        addE("knows").from("marko").to("josh").property("weight", 1.0d).
+        addE("created").from("marko").to("lop").property("weight", 0.4d).
+        addE("created").from("josh").to("ripple").property("weight", 1.0d).
+        addE("created").from("josh").to("lop").property("weight", 0.4d).
+        addE("created").from("peter").to("lop").property("weight", 0.2d).iterate();
+
+        List<Vertex> vertices =  this.sqlgGraph.traversal().V().addV("animal").property("age", 0).toList();
+        Assert.assertEquals(6, vertices.size());
+
+    }
 
     @Test
     public void g_addVXV_hasXname_markoX_propertiesXnameX_keyX_label() {
