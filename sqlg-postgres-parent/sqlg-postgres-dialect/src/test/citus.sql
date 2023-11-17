6e2013680cafd58adb03e4312fd6e67c00161369
pieter martin <pieter.martin@riseup.net>
2023-11-17 13:46:45 +0200

    update dependency versions

--- sqlg-postgres-parent/sqlg-postgres-dialect/src/test/citus.sql
+++ sqlg-postgres-parent/sqlg-postgres-dialect/src/test/citus.sql
@@ -3,37 +3,38 @@
 cd /var/lib/postgres/
 mkdir coordinator
 chown -R postgres:postgres coordinator
-#as postgres
-initdb -D /var/lib/postgres/coordinator
-pg_ctl -D /var/lib/postgres/coordinator -o "-p 9700" start
-
-cd /var/lib/postgres/
 mkdir worker1
 chown -R postgres:postgres worker1
-#as postgres
-initdb -D /var/lib/postgres/worker1
-pg_ctl -D /var/lib/postgres/worker1 -o "-p 9701" start
-
-cd /var/lib/postgres/
 mkdir worker2
-chown -R postgres:postgres worker2
-#as postgres
-initdb -D /var/lib/postgres/worker2
-pg_ctl -D /var/lib/postgres/worker2 -o "-p 9702" start
-
-cd /var/lib/postgres/
+chown -R postgres:postgres worker3
 mkdir worker3
 chown -R postgres:postgres worker3
-#as postgres
-initdb -D /var/lib/postgres/worker3
-pg_ctl -D /var/lib/postgres/worker3 -o "-p 9703" start
-
-cd /var/lib/postgres/
 mkdir worker4
 chown -R postgres:postgres worker4
+
 #as postgres
+initdb -D /var/lib/postgres/coordinator
+initdb -D /var/lib/postgres/worker1
+initdb -D /var/lib/postgres/worker2
+initdb -D /var/lib/postgres/worker3
 initdb -D /var/lib/postgres/worker4
+
+#as root
+echo "shared_preload_libraries = 'citus'" | sudo tee -a /var/lib/postgres/coordinator/postgresql.conf
+echo "shared_preload_libraries = 'citus'" | sudo tee -a /var/lib/postgres/worker1/postgresql.conf
+echo "shared_preload_libraries = 'citus'" | sudo tee -a /var/lib/postgres/worker2/postgresql.conf
+echo "shared_preload_libraries = 'citus'" | sudo tee -a /var/lib/postgres/worker4/postgresql.conf
+echo "shared_preload_libraries = 'citus'" | sudo tee -a /var/lib/postgres/worker4/postgresql.conf
+
+#as postgres
+pg_ctl -D /var/lib/postgres/coordinator -o "-p 9700" start
+pg_ctl -D /var/lib/postgres/worker1 -o "-p 9701" start
+pg_ctl -D /var/lib/postgres/worker2 -o "-p 9702" start
+pg_ctl -D /var/lib/postgres/worker3 -o "-p 9703" start
 pg_ctl -D /var/lib/postgres/worker4 -o "-p 9704" start
+
+#on all nodes
+create extension citus;
 
 #on the coordinator
 SELECT * from citus_add_node('localhost', 9701);
@@ -41,9 +42,3 @@ SELECT * from citus_add_node('localhost', 9702);
 SELECT * from citus_add_node('localhost', 9703);
 SELECT * from citus_add_node('localhost', 9704);
 SELECT * FROM citus_get_active_worker_nodes();
-
-pg_ctl -D coordinator -o "-p 9700"  start
-pg_ctl -D worker1 -o "-p 9701"  start
-pg_ctl -D worker2 -o "-p 9702"  start
-pg_ctl -D worker3 -o "-p 9703"  start
-pg_ctl -D worker4 -o "-p 9704"  start
