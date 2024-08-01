7f866635691eb9b7b2c52b6167a183dbc5a79d46
pieter <pieter.martin@riseup.net>
2024-08-01 09:26:06 +0200

    added back mysql support

---
+++ sqlg-mysql-parent/sqlg-mysql/src/test/java/org/umlg/sqlg/test/MysqlAnyTest.java
@@ -0,0 +1,9 @@
+package org.umlg.sqlg.test;
+
+/**
+ * @author <a href="https://github.com/pietermartin">Pieter Martin</a>
+ * @author Vasili Pispanen
+ *         Date: 2018/05/22
+ */
+public class MysqlAnyTest extends AnyTest {
+}
