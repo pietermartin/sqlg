fe14fd54830282270c838233dc063c73c6c3f2c6
pieter <pieter.martin@riseup.net>
2024-10-16 22:15:41 +0200

    test for local startup optimizations

--- sqlg-test/src/main/java/org/umlg/sqlg/test/TestTest.java
+++ sqlg-test/src/main/java/org/umlg/sqlg/test/TestTest.java
@@ -1,109 +1,37 @@
 package org.umlg.sqlg.test;
 
-import org.apache.tinkerpop.gremlin.process.traversal.Path;
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
-import org.apache.tinkerpop.gremlin.structure.Edge;
-import org.apache.tinkerpop.gremlin.structure.T;
-import org.apache.tinkerpop.gremlin.structure.Vertex;
-import org.junit.Assert;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.umlg.sqlg.structure.Multiplicity;
-import org.umlg.sqlg.structure.PropertyDefinition;
-import org.umlg.sqlg.structure.PropertyType;
-import org.umlg.sqlg.structure.topology.EdgeDefinition;
-import org.umlg.sqlg.structure.topology.EdgeLabel;
-import org.umlg.sqlg.structure.topology.VertexLabel;
+import org.umlg.sqlg.structure.SqlgGraph;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-
-public class TestTest extends BaseTest {
+public class TestTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TestTest.class);
 
-    //    @BeforeClass
-//    public static void beforeClass() {
-//        URL sqlProperties = Thread.currentThread().getContextClassLoader().getResource("sqlg.properties");
-//        try {
-//            Configurations configs = new Configurations();
-//            configuration = configs.properties(sqlProperties);
-//            Assume.assumeTrue(isPostgres());
-//            configuration.addProperty(SqlgGraph.DISTRIBUTED, true);
-//            if (!configuration.containsKey(SqlgGraph.JDBC_URL))
-//                throw new IllegalArgumentException(String.format("SqlGraph configuration requires that the %s be set", SqlgGraph.JDBC_URL));
-//
-//        } catch (ConfigurationException e) {
-//            throw new IllegalStateException(e);
-//        }
-//    }
+    @BeforeClass
+    public static void beforeClass() {
+        PropertiesConfiguration configuration = new PropertiesConfiguration();
+        configuration.addProperty("jdbc.url", "jdbc:postgresql://localhost:5432/cm-dist");
+//        configuration.addProperty("jdbc.url", "jdbc:postgresql://10.70.41.151:5432/cm");
+        configuration.addProperty("jdbc.username", "cm");
+        configuration.addProperty("jdbc.password", "cm");
+        SqlgGraph sqlgGraph = SqlgGraph.open(configuration);
+        sqlgGraph.close();
+    }
 
-    @Test
-    public void testFriendOfFriend() {
-        VertexLabel friendVertexLabel = this.sqlgGraph.getTopology().getPublicSchema().ensureVertexLabelExist("Friend", new LinkedHashMap<>() {{
-            put("name", PropertyDefinition.of(PropertyType.STRING, Multiplicity.of(1, 1)));
-        }});
-        friendVertexLabel.ensureEdgeLabelExist(
-                "of",
-                friendVertexLabel,
-                EdgeDefinition.of(
-                        Multiplicity.of(0, -1),
-                        Multiplicity.of(0, -1)
-                )
-        );
-        this.sqlgGraph.tx().commit();
-        this.sqlgGraph.getTopology().lock();
-
-        Vertex john = sqlgGraph.addVertex(T.label, "Friend", "name", "John");
-        Vertex peter = sqlgGraph.addVertex(T.label, "Friend", "name", "Peter");
-        Vertex dave = sqlgGraph.addVertex(T.label, "Friend", "name", "Dave");
-        Vertex mike = sqlgGraph.addVertex(T.label, "Friend", "name", "Mike");
-        Vertex luke = sqlgGraph.addVertex(T.label, "Friend", "name", "Luke");
-        
-        john.addEdge("of", peter);
-        peter.addEdge("of", dave);
-        dave.addEdge("of", mike);
-        mike.addEdge("of", luke);
-        this.sqlgGraph.tx().commit();
-
-        List<String> friendsName = this.sqlgGraph.traversal().V().hasLabel("Friend")
-                .<String>values("name")
-                .toList();
-        Assert.assertEquals(4, friendsName.size());
-        List<Vertex> friends = this.sqlgGraph.traversal().V().hasLabel("Friend")
-                .toList();
-        Assert.assertEquals(4, friends.size());
-        Vertex friend = friends.get(0);
-        Assert.assertEquals("John", friend.property("name").value());
-
-        GraphTraversal<Vertex, Path> graphTraversal = this.sqlgGraph.traversal().V().hasLabel("Friend").has("name", "Peter")
-                .repeat(__.both("of").simplePath()).until(__.not(__.both("of").simplePath()))
-                .path();
-        List<Path> paths = graphTraversal.toList();
-        Assert.assertEquals(2, paths.size());
-        Assert.assertTrue(paths.stream().anyMatch(p -> p.size() == 2 && p.get(0).equals(peter) && p.get(1).equals(john)));
+    @Before
+    public void before() throws Exception {
 
     }
 
-    //    @Test
-    public void test3() {
-        loadModern();
-        for (EdgeLabel edgeLabel : this.sqlgGraph.getTopology().getPublicSchema().getEdgeLabels().values()) {
-            edgeLabel.remove();
-        }
-        for (VertexLabel vertexLabel : this.sqlgGraph.getTopology().getPublicSchema().getVertexLabels().values()) {
-            vertexLabel.remove();
-        }
-        Vertex a = this.sqlgGraph.addVertex(T.label, "a");
-        Vertex b = this.sqlgGraph.addVertex(T.label, "b");
-        a.addEdge("knows", b);
-        this.sqlgGraph.traversal().V().drop().iterate();
-        List<Edge> edges = this.sqlgGraph.traversal().E().hasLabel("knows").toList();
-        Assert.assertEquals(1, edges.size());
+    @Test
+    public void test() {
 
-        this.sqlgGraph.tx().commit();
+
     }
 
 }
